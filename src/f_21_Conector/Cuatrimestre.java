2ef71b0ef65b3c5563edaab57b4be8232c0b9e3b
Aaron Santos <asantosabsalon@gmail.com>
2024-07-22 10:50:08 -0500

    New features and files: Cuatrimestre y relacionados añadidos, junto con correcciones

---
+++ src/f_21_Conector/Cuatrimestre.java
@@ -0,0 +1,38 @@
+package f_21_Conector;
+
+//! Codigo ya copiado
+
+public class Cuatrimestre {
+
+    // Atributo que almacena el ID del cuatrimestre
+    private int id;
+    // Atributo que almacena el nombre del cuatrimestre
+    private String nombre;
+
+    // Constructor que inicializa un cuatrimestre con un ID y un nombre
+    public Cuatrimestre(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // Método para obtener el ID del cuatrimestre
+    public int getId() {
+        return id;
+    }
+
+    // Método para obtener el nombre del cuatrimestre
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Método para establecer el ID del cuatrimestre
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Método para establecer el nombre del cuatrimestre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
